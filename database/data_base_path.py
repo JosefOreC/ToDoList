07063d650d156c459adbcabe9615a1da08b2b4eb
JosefOre <71660137@continental.edu.pe>
2025-06-23 04:55:34 -0500

    Se han eliminado archivos innecesarios

--- database/data_base_path.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-class Recover:
-    conn = sqlite3.connect("../src/modelo/database_management/todolist_db.db")
-    def recovering(self):
-        return self.conn
-    def recover_cursor(self):
-        return self.conn.cursor()
-if __name__=='__main__':
-    pass
